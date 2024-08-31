b98b5f8b76c548ed3e0c6fdd33ce198b900ed38c
Uzair Mansoor <82077348+uzairmansoor@users.noreply.github.com>
2024-08-31 11:05:06 +0500

    Update app.js
    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Congratulations! This deployment is done through CodePipeline on Elastic Beantsalk!'));
+app.get('/', (req, res) => res.send('Hurrah! This deployment is done through CodePipeline on Elastic Beantsalk!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
